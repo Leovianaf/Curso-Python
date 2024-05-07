c4eab9686aec384e0d39c11565aa288521f234bf
Leonardo Viana <leonardoviana.nave@gmail.com>
2024-05-07 15:51:32 -0300

    Aula 397

--- aula387/main.py
+++ aula387/main.py
@@ -3,12 +3,27 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite3'
-DB_PATH = ROOT_DIR / DB_NAME
+DB_FILE = ROOT_DIR / DB_NAME
 TABLE_NAME = 'customers'
 
-connection = sqlite3.connect(DB_PATH)
+connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+# CRUD - Create Read   Update Delete
+# SQL -  INSERT SELECT UPDATE DELETE
+
+# CUIDADO: Fazendo delete sem where
+cursor.execute(
+  f'DELETE FROM {TABLE_NAME}'
+)
+
+# DELETE mais cuidadoso (com where)
+cursor.execute(
+  f'DELETE FROM sqlite_sequence WHERE name = "{TABLE_NAME}"'
+)
+connection.commit()
+
+# Cria a tabela
 cursor.execute(
   f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
   '('
@@ -16,6 +31,13 @@ cursor.execute(
   'name TEXT,'
   'weight REAL'
   ')'
+)
+connection.commit()
+
+# Registrar valores nas colunas da tabela
+cursor.executemany(
+  f'INSERT INTO {TABLE_NAME} (name, weight) VALUES (?, ?)',
+  [('João', 70), ('Maria', 50), ('José', 80)]
 )
 connection.commit()
 
